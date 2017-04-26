7e92e331c2ad0ea5fa956c6e5a11bc65e322995d
zh99998 <zh99998@gmail.com>
2017-04-26 15:48:41 +0800

    commonjs

--- index.js
+++ index.js
@@ -1,54 +1,44 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const compiler_1 = require("@angular/compiler");
+const tags = {
+    webview: {
+        src: true,
+        autosize: true,
+        nodeintegration: true,
+        plugins: true,
+        preload: true,
+        httpreferrer: true,
+        useragent: true,
+        disablewebsecurity: true,
+        partition: true,
+        allowpopups: true,
+        webpreferences: true,
+        blinkfeatures: true,
+        disableblinkfeatures: true,
+        guestinstance: true,
+        disableguestresize: true
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "@angular/compiler"], factory);
+};
+exports.ELECTRON_SCHEMA = {
+    name: 'electron-schema'
+};
+compiler_1.DomElementSchemaRegistry.prototype.hasElement = new Proxy(compiler_1.DomElementSchemaRegistry.prototype.hasElement, {
+    apply(target, thisArgument, argumentsList) {
+        const [tagName, schemaMetas] = argumentsList;
+        if (schemaMetas.some((schema) => schema.name === exports.ELECTRON_SCHEMA.name) && tags[tagName]) {
+            return true;
+        }
+        return Reflect.apply(target, thisArgument, argumentsList);
     }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    const compiler_1 = require("@angular/compiler");
-    const tags = {
-        webview: {
-            src: true,
-            autosize: true,
-            nodeintegration: true,
-            plugins: true,
-            preload: true,
-            httpreferrer: true,
-            useragent: true,
-            disablewebsecurity: true,
-            partition: true,
-            allowpopups: true,
-            webpreferences: true,
-            blinkfeatures: true,
-            disableblinkfeatures: true,
-            guestinstance: true,
-            disableguestresize: true
+});
+compiler_1.DomElementSchemaRegistry.prototype.hasProperty = new Proxy(compiler_1.DomElementSchemaRegistry.prototype.hasProperty, {
+    apply(target, thisArgument, argumentsList) {
+        const [tagName, propName, schemaMetas] = argumentsList;
+        if (schemaMetas.some((schema) => schema.name === exports.ELECTRON_SCHEMA.name) && tags[tagName] && tags[tagName][propName]) {
+            return true;
         }
-    };
-    exports.ELECTRON_SCHEMA = {
-        name: 'electron-schema'
-    };
-    compiler_1.DomElementSchemaRegistry.prototype.hasElement = new Proxy(compiler_1.DomElementSchemaRegistry.prototype.hasElement, {
-        apply(target, thisArgument, argumentsList) {
-            const [tagName, schemaMetas] = argumentsList;
-            if (schemaMetas.some((schema) => schema.name === exports.ELECTRON_SCHEMA.name) && tags[tagName]) {
-                return true;
-            }
-            return Reflect.apply(target, thisArgument, argumentsList);
-        }
-    });
-    compiler_1.DomElementSchemaRegistry.prototype.hasProperty = new Proxy(compiler_1.DomElementSchemaRegistry.prototype.hasProperty, {
-        apply(target, thisArgument, argumentsList) {
-            const [tagName, propName, schemaMetas] = argumentsList;
-            if (schemaMetas.some((schema) => schema.name === exports.ELECTRON_SCHEMA.name) && tags[tagName] && tags[tagName][propName]) {
-                return true;
-            }
-            return Reflect.apply(target, thisArgument, argumentsList);
-        }
-    });
+        return Reflect.apply(target, thisArgument, argumentsList);
+    }
 });
 //# sourceMappingURL=index.js.map
